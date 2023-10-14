495b044075774288394d1b4de6590de0dd5e1b25
Guido M Mantilla T <guido.mantilla@yahoo.com>
2023-10-14 12:10:49 -0500

    update

--- pkg/sql/builder.go
+++ pkg/sql/builder.go
@@ -1,6 +1,8 @@
 package sql
 
-func CreateSelectSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) (string, error) {
+type CreateStatementSQLFunc func(table string, value any, driverName DriverName, paramHolder ParamHolder, fn ColumnFilterFunc) string
+
+func CreateSelectSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) string {
 
 	empty := ""
 	separator := ", "
@@ -8,23 +10,23 @@ func CreateSelectSQL(table string, value any, _ DriverName, paramHolder ParamHol
 	var err error
 	var sequence string
 	if sequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, 0, ColumnFilter, EvalNameOnly); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
 	if fn01 == nil {
-		return "SELECT " + sequence + " FROM " + table, nil
+		return "SELECT " + sequence + " FROM " + table
 	}
 
 	separator = " AND "
 	var whereSequence string
 	if whereSequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, 0, fn01, paramHolder.EvalNameValue()); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
-	return "SELECT " + sequence + " FROM " + table + " WHERE " + whereSequence, nil
+	return "SELECT " + sequence + " FROM " + table + " WHERE " + whereSequence
 }
 
-func CreateInsertSQL(table string, value any, driverName DriverName, paramHolder ParamHolder) (string, error) {
+func CreateInsertSQL(table string, value any, driverName DriverName, paramHolder ParamHolder, _ ColumnFilterFunc) string {
 
 	initChar := "("
 	endChar := ")"
@@ -34,37 +36,37 @@ func CreateInsertSQL(table string, value any, driverName DriverName, paramHolder
 	var err error
 	var nameSequence string
 	if nameSequence, _, err = ParseColumnAsNameValueSequence(value, initChar, endChar, separator, 0, NonePkGeneratedColumnFilter, EvalNameOnly); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
 	var cont int
 	var valueSequence string
 	if valueSequence, cont, err = ParseColumnAsNameValueSequence(value, initChar, endChar, separator, 0, NonePkGeneratedColumnFilter, paramHolder.EvalValueOnly()); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
 	var returning string
 	if driverName == OracleDriverName {
 		var pkNameSequence string
 		if pkNameSequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, cont, PkGeneratedColumnFilter, EvalNameOnly); err != nil {
-			return "", ErrSQLGenerationFailed(err)
+			panic(ErrSQLGenerationFailed(err))
 		}
 
 		var pkValueSequence string
 		if pkValueSequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, cont, PkGeneratedColumnFilter, paramHolder.EvalValueOnly()); err != nil {
-			return "", ErrSQLGenerationFailed(err)
+			panic(ErrSQLGenerationFailed(err))
 		}
 		returning = " RETURNING " + pkNameSequence + " INTO " + pkValueSequence
 	}
 
-	return "INSERT INTO " + table + " " + nameSequence + " VALUES " + valueSequence + returning, nil
+	return "INSERT INTO " + table + " " + nameSequence + " VALUES " + valueSequence + returning
 }
 
-func CreateUpsertSQL(table string, value any, driverName DriverName, paramHolder ParamHolder) (string, error) {
-	return "TO IMPLEMENT", nil
+func CreateUpsertSQL(table string, value any, driverName DriverName, paramHolder ParamHolder, _ ColumnFilterFunc) string {
+	return "TO IMPLEMENT"
 }
 
-func CreateUpdateSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) (string, error) {
+func CreateUpdateSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) string {
 
 	separator := ", "
 	empty := ""
@@ -73,18 +75,18 @@ func CreateUpdateSQL(table string, value any, _ DriverName, paramHolder ParamHol
 	var cont int
 	var nameSequence string
 	if nameSequence, cont, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, 0, ColumnFilter, paramHolder.EvalNameValue()); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
 	var whereSequence string
 	if whereSequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, cont, fn01, paramHolder.EvalNameValue()); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
-	return "UPDATE " + table + " SET " + nameSequence + " WHERE " + whereSequence, nil
+	return "UPDATE " + table + " SET " + nameSequence + " WHERE " + whereSequence
 }
 
-func CreateDeleteSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) (string, error) {
+func CreateDeleteSQL(table string, value any, _ DriverName, paramHolder ParamHolder, fn01 ColumnFilterFunc) string {
 
 	separator := ", "
 	empty := ""
@@ -92,8 +94,8 @@ func CreateDeleteSQL(table string, value any, _ DriverName, paramHolder ParamHol
 	var err error
 	var valueSequence string
 	if valueSequence, _, err = ParseColumnAsNameValueSequence(value, empty, empty, separator, 0, fn01, paramHolder.EvalNameValue()); err != nil {
-		return "", ErrSQLGenerationFailed(err)
+		panic(ErrSQLGenerationFailed(err))
 	}
 
-	return "DELETE FROM " + table + " WHERE " + valueSequence, nil
+	return "DELETE FROM " + table + " WHERE " + valueSequence
 }
